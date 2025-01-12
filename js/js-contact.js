4191f6cef21671b3be8a3c0271e1180130f92917
thierry-vanboxel <thierryvanboxel@hotmail.com>
2025-01-12 09:32:42 +0100

    1

---
+++ js/js-contact.js
@@ -0,0 +1,14 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = currentYear;
+
+
+let kaart = L.map('kaart').setView([51.171111, 4.455833], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(kaart);
+
+L.marker([51.171111, 4.455833]).addTo(kaart)
+    .bindPopup('Mortsel')
+    .openPopup();
+
