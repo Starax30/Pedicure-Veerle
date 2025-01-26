a2b8f72fc8112643af8320cbf7bdb92a03740a1c
thierry-vanboxel <thierryvanboxel@hotmail.com>
2025-01-26 11:21:56 +0100

    1

--- js/js-index.js
+++ js/js-index.js
@@ -14,23 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Interactieve tijdlijn geladen!");
   });
   
-  const map = L.map('map').setView([51.2194, 4.4025], 13); // Antwerpen
+  const map = L.map('map').setView([51.3482122, 4.7606187], 11); // Antwerpen
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '© OpenStreetMap contributors'
   }).addTo(map);
 
-  // Voeg een marker toe
-  L.marker([51.2194, 4.4025]).addTo(map)
-      .bindPopup("Locatie Pedicure Veerle")
-      .openPopup();
 
-      
 const polygon = L.polygon([
-  [52.3702, 4.8952],  // Punt 1
-  [52.3712, 4.8962],  // Punt 2
-  [52.3722, 4.8942],  // Punt 3
-  [52.3702, 4.8952]   // Terug naar Punt 1
+  [51.4213173,4.5395157 ],  // Punt 1
+  [51.3834342,5.0768168],  // Punt 2
+  [51.2859158,4.9834507],
+  [51.2859158,4.9834507],  // Punt 3
+  [51.2900898,4.4915345],
+  [51.4213173,4.5395157 ]   // Terug naar Punt 1
 ], {
   color: 'blue',      // Randkleur
   fillColor: 'skyblue', // Vul kleur
@@ -40,29 +37,6 @@ const polygon = L.polygon([
 // Optioneel: Een popup toevoegen
 polygon.bindPopup("Dit is een aangeduid gebied.");
 
-const circle = L.circle([52.3702, 4.8952], {
-  color: 'red',       // Randkleur
-  fillColor: 'pink',  // Vul kleur
-  fillOpacity: 0.5,   // Transparantie
-  radius: 500         // Straal in meters
-}).addTo(map);
 
-circle.bindPopup("Dit is een cirkelgebied.");
-
-const rectangle = L.rectangle([
-  [52.3690, 4.8930],  // Zuidwest
-  [52.3720, 4.8970]   // Noordoost
-], {
-  color: 'green',
-  fillColor: 'lightgreen',
-  fillOpacity: 0.5
-}).addTo(map);
-
-rectangle.bindPopup("Dit is een rechthoekig gebied.");
-
-
-polygon.addTo(map);
-circle.addTo(map);
-rectangle.addTo(map);
 
 
